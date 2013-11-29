19350eaac84ec471ed85acba41674887480fc4e3
Marius Mandal <mariusmandal@gmail.com>
2013-11-29 10:23:01 +0100

    filter roles name

--- ukmwp_admin-tweaks.php
+++ ukmwp_admin-tweaks.php
@@ -65,5 +65,5 @@ require_once('tweak.capabilities.php');
 ## CHANGE ROLE NAMES
 add_action('init', 'UKMwpat_change_role_name');
 add_action('admin_init', 'UKMwpat_change_role_name');
-add_action('members_get_capabilities', 'UKMwpat_change_role_name');
+add_action('UKM_filter_roles', 'UKMwpat_change_role_name');
 add_action('wp_before_admin_bar_render','UKMwpat_modify_toolbar', 10000);
