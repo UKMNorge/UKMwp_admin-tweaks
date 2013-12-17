5db767d284e9d50e6b47bc34ae4f6e808d244ee7
Marius Mandal <mariusmandal@gmail.com>
2013-12-17 18:23:17 +0100

    Fjern tools
    Har fjernet menu-plugin (admin-tweaks har nå all denne funksjonaliteten)

--- tweak.menu.php
+++ tweak.menu.php
@@ -2,22 +2,10 @@
 function UKMwpat_tweak_menu_remove() {
 	global $current_user, $menu, $submenu;
 
+	// RENAME
 	$menu[2][0] = 'Startside';
 	
-	$remove = array(10	=> 'upload.php',
-					15	=> 'edit-tags.php?taxonomy=link_category',
-					20	=> 'edit.php?post_type=page',
-					25	=> 'edit-comments.php',
-					);	
-	foreach( $remove as $id => $file) {
-		if( $menu[ $id ][2] == $file )
-			unset( $menu[ $id ] );	
-	}
-	
-	remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=category' );
-	remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=post_tag' );
-
-		
+	// MOVE
 	$move = array(59 => 'separator2',
 				  60 => 'themes.php',
 				  65 => 'plugins.php',
@@ -32,6 +20,22 @@ function UKMwpat_tweak_menu_remove() {
 		}
 	}
 	
+	// REMOVE
+	$remove = array(10	=> 'upload.php',
+					15	=> 'edit-tags.php?taxonomy=link_category',
+					20	=> 'edit.php?post_type=page',
+					25	=> 'edit-comments.php',
+					);	
+	if( !is_super_admin() ) 
+		$remove[1075] = 'tools.php';
+	foreach( $remove as $id => $file) {
+		if( $menu[ $id ][2] == $file )
+			unset( $menu[ $id ] );	
+	}
+	
+	remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=category' );
+	remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=post_tag' );
+
 	// Hide profile menu (use the right top corner instead)
 	if( $menu[70][2] == 'profile.php') {
 		unset($menu[70]);
