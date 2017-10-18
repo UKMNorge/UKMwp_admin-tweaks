517f010fdbf5acd0aa8944ba709ed7303a966f26
Marius Mandal <mariusmandal@gmail.com>
2017-10-18 21:19:42 +0200

    https på login logo

--- tweak.login.php
+++ tweak.login.php
@@ -2,7 +2,7 @@
 
 function UKMwpat_login() {
 	echo '<style type="text/css">
-		h1 a {background-image:url(http://grafikk.ukm.no/profil/logo/UKM_logo.png) !important; background-size: 140px 80px !important; min-height: 80px;
+		h1 a {background-image:url(//grafikk.ukm.no/profil/logo/UKM_logo.png) !important; background-size: 140px 80px !important; min-height: 80px;
     min-width: 160px; margin:0 auto;}
 		</style>';
 
