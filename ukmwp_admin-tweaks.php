4800b69d7f69d389bdb8358f6ac70210a7778d2d
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-10-25 13:47:24 +0200

    Update ukmwp_admin-tweaks.php

--- ukmwp_admin-tweaks.php
+++ ukmwp_admin-tweaks.php
@@ -35,13 +35,7 @@ add_action('show_user_profile', 'UKMwpat_profile_deactivated_warning');
 add_action('edit_user_profile', 'UKMwpat_profile_deactivated_warning');
 add_filter( 'user_contactmethods', 'UKMwpat_profile_fields' );
 add_filter( "user_user_phone_label", 'UKMwpat_profile_field_phone_description' );
-add_filter('manage_edit-wp-application-passwords_columns', 'custom_hide_application_password_columns');
-
-function custom_hide_application_password_columns($columns) {
-    // Remove the 'Actions' column to hide application passwords.
-    unset($columns['actions']);
-    return $columns;
-}
+add_filter('wp_is_application_passwords_available', '__return_false');
 
 
 function UKMwpat_profile_field_phone_description( $user ){
