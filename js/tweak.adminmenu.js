b6ccca3715efd4a15b808f013e6c44cc2b470f0f
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-04-11 16:56:19 +0200

    Endringer på min side
    Click on mobile fikset
    Endringer på CSS
    Fjernet arrangement side på meny panel

--- js/tweak.adminmenu.js
+++ js/tweak.adminmenu.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function() {
     jQuery('.dashicons-wordpress-pink').addClass('dashicons-admin-tools');
+
+
+    // Fikser click på meny på bruker panel når det er mobil
+    jQuery('#wpadminbar.mobile .quicklinks #wp-admin-bar-root-default #wp-admin-bar-wp-logo').on('click', function(event) {
+        // Redirect to the URL
+        window.location.href = jQuery(event.currentTarget).find('a.ab-item').attr('href');
+
+    });
+    
 });
